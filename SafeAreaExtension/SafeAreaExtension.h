b47d09dfafc960d0db7278b64d4d548767741495
myoungjunkim <myoungjun.kim@gmail.com>
2017-10-17 14:48:27 +0900

    Initial Commit

---
+++ SafeAreaExtension/SafeAreaExtension.h
@@ -0,0 +1,19 @@
+//
+//  SafeAreaExtension.h
+//  SafeAreaExtension
+//
+//  Created by Myoungjun Kim on 2017. 10. 17..
+//  Copyright © 2017년 Myoungjun Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeAreaExtension.
+FOUNDATION_EXPORT double SafeAreaExtensionVersionNumber;
+
+//! Project version string for SafeAreaExtension.
+FOUNDATION_EXPORT const unsigned char SafeAreaExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeAreaExtension/PublicHeader.h>
+
+
